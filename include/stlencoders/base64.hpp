5f77c5f7b2ec10e0f9cc515fbc5a0cb080cde166
thomas.h.kemmer@gmail.com <thomas.h.kemmer@gmail.com@534ea1f5-b13c-0be0-43e0-443b30af858b>
2012-05-22 17:19:12 +0000

    removed hosting info from copyright statements
    git-svn-id: https://stlencoders.googlecode.com/svn/trunk@209 534ea1f5-b13c-0be0-43e0-443b30af858b

--- include/stlencoders/base64.hpp
+++ include/stlencoders/base64.hpp
@@ -1,8 +1,6 @@
 /*
  * Copyright (c) 2011, 2012 Thomas Kemmer <tkemmer@computer.org>
  *
- * http://code.google.com/p/stlencoders/
- *
  * Permission is hereby granted, free of charge, to any person
  * obtaining a copy of this software and associated documentation
  * files (the "Software"), to deal in the Software without
